ce032cde43bd1002ec4b69981acf270fe089c09d
YaaaaaaY-B <devanshvadgama3@gmail.com>
2024-07-30 21:07:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function openLightbox(src) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    lightboxImg.src = src;
+    lightbox.style.display = 'flex';
+}
+
+function closeLightbox() {
+    const lightbox = document.getElementById('lightbox');
+    lightbox.style.display = 'none';
+}
+
+
